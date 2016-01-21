6c668262f0d991fe15e1c6c78f59cf16bc4f598c
Quentan <quentan@gmail.com>
2016-01-21 19:26:29 +0000

    vtkImageData can be modified by its SetScalarComponentFromDouble() method

--- Playground.py
+++ Playground.py
@@ -7,6 +7,7 @@ import slicer
 from slicer.ScriptedLoadableModule import *
 import logging
 
+import math
 
 class Playground(ScriptedLoadableModule):
 
@@ -55,3 +56,37 @@ class PlaygroundWidget(ScriptedLoadableModuleWidget):
 
         # qt.QMessageBox.information(slicer.util.mainWindow(),
         #                            "TEST Window", "For test only!")
+
+        volumeNode = slicer.util.getNode('MRHead')
+        ijkToRas = vtk.vtkMatrix4x4()
+        volumeNode.GetIJKToRASMatrix(ijkToRas)
+        imageData = volumeNode.GetImageData()
+        extent = imageData.GetExtent()
+
+        image_np = slicer.util.array('MRHead')
+
+        for k in xrange(extent[4], extent[5]/2+1):
+            for j in xrange(extent[2], extent[3]/2+1):
+                for i in xrange(extent[0], extent[1]/2+1):
+                    position_Ijk = [i, j, k, 1]
+                    position_Ras = ijkToRas.MultiplyPoint(position_Ijk)
+
+                    r = position_Ras[0]
+                    a = position_Ras[1]
+                    s = position_Ras[2]
+
+                    # print "r=%f, a=%f, s=%f, Value[r, a, s]=%f" % (r, a, s, image_np[r, a, s])
+                    # print "i=%f, j=%f, k=%f, Value[i, j, k]=%f" % (i, j, k, image_np[k, j, i])
+
+                    functionValue = (r-10)*(r-10) + (a+15)*(a+15) + s*s
+                    # functionValue = math.sqrt((r-10)**2 + (a+15)**2 + s**2)
+                    # functionValue = r-10 + a+15 + s
+                    functionValue += image_np[k, j, i]
+                    imageData.SetScalarComponentFromDouble(
+                        i, j, k, 0, functionValue)
+
+        # imageData.SetScalarComponentFromFloat(distortionVerctorPosition_Ijk[0],
+        #                                       distortionVerctorPosition_Ijk[1],
+        #                                       distortionVerctorPosition_Ijk[2],
+        #                                       0, fillValue)
+        imageData.Modified()
