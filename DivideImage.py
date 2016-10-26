7de3a8d6cde4bc4fdbd798435c5a1aae94c52be2
Quentan <quentan@gmail.com>
2016-10-26 14:30:33 +0800

    A complete basic vtk rendering is tested

--- DivideImage.py
+++ DivideImage.py
@@ -811,9 +811,10 @@ class DivideImageTest(ScriptedLoadableModuleTest):
         self.setUp()
         # self.test1_DivideImage()
         # self.test2_DivideImage()
-        self.test3_DivideImage()
+        # self.test3_DivideImage()
         # self.test4_DivideImage()
         # self.test_EmptyVolume()
+        self.test_Vtk()
 
     def test1_DivideImage(self):
         """
@@ -937,3 +938,57 @@ class DivideImageTest(ScriptedLoadableModuleTest):
         displayNode.SetAndObserveColorNodeID(colorNode.GetID())
         volumeNode.SetAndObserveDisplayNodeID(displayNode.GetID())
         volumeNode.CreateDefaultStorageNode()
+
+    def test_Vtk(self):
+        """
+        Test basic VTK rendering in Slicer.
+        This code can also be run independet
+        """
+        color_diffuse = [247.0 / 255.0, 150.0 / 255.0, 155.0 / 255.0]
+        # This creates a polygonal cylinder model with eight circumferential
+        # facets.
+        cylinder = vtk.vtkCylinderSource()
+        cylinder.SetResolution(8)
+
+        # The mapper is responsible for pushing the geometry into the graphics
+        # library. It may also do color mapping, if scalars or other
+        # attributes are defined.
+        cylinderMapper = vtk.vtkPolyDataMapper()
+        cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
+
+        # The actor is a grouping mechanism: besides the geometry (mapper), it
+        # also has a property, transformation matrix, and/or texture map.
+        # Here we set its color and rotate it -22.5 degrees.
+        cylinderActor = vtk.vtkActor()
+        cylinderActor.SetMapper(cylinderMapper)
+        cylinderActor.GetProperty().SetColor(color_diffuse)
+        cylinderActor.RotateX(30.0)
+        cylinderActor.RotateY(-45.0)
+
+        # Create the graphics structure. The renderer renders into the render
+        # window. The render window interactor captures mouse events and will
+        # perform appropriate camera or actor manipulation depending on the
+        # nature of the events.
+        ren = vtk.vtkRenderer()
+        renWin = vtk.vtkRenderWindow()
+        renWin.AddRenderer(ren)
+        iren = vtk.vtkRenderWindowInteractor()
+        iren.SetRenderWindow(renWin)
+
+        # Add the actors to the renderer, set the background and size
+        ren.AddActor(cylinderActor)
+        ren.SetBackground(0.2, 0.3, 0.4)
+        renWin.SetSize(200, 200)
+
+        # This allows the interactor to initalize itself. It has to be
+        # called before an event loop.
+        iren.Initialize()
+
+        # We'll zoom in a little by accessing the camera and invoking a "Zoom"
+        # method on it.
+        ren.ResetCamera()
+        ren.GetActiveCamera().Zoom(1.5)
+        renWin.Render()
+
+        # Start the event loop.
+        iren.Start()
